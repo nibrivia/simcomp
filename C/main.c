e95a213f05ba4f3c79dbd6372d8b072d4cf495b3
Olivia Brode-Roger <nibr@mit.edu>
2020-05-05 23:33:53 -0400

    C stub

---
+++ C/main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+struct Packet {
+    int src;
+    int dst;
+};
+
+struct Flow {
+};
+
+struct Event {
+    int time;
+};
+
+
+// TODO heap...
+
+int main() {
+    printf("Hello world!");
+    return 0;
+}
+
+
+// NIC rx
